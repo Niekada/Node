d843db77e371333681ef2829cbf3176513d28e52
Niekada <erik.tankeliun@gmail.com>
2022-12-05 21:33:54 +0200

    pirma paskaita

---
+++ 1_paskaita/index.js
@@ -0,0 +1,5 @@
+const casual = require('casual');
+
+console.log(casual.city);
+console.log(Math.floor(Math.random() * 10));
+console.log(`${casual.name_suffix} ${casual.first_name} ${casual.last_name}`)
